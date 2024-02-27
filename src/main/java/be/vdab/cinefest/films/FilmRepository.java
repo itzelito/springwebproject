fdf2343fcbae60598966abe4c1a9e9cd1f913f6e
ItzelMaldonado <itzel.mn@gmail.com>
2024-02-27 11:43:43 +0100

    repository class filmRepository om naar de database te roepen met een methode findTotaalVrijePlaatsen en zijn eigen service class om die aan te spreken

---
+++ src/main/java/be/vdab/cinefest/films/FilmRepository.java
@@ -0,0 +1,28 @@
+package be.vdab.cinefest.films;
+
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+class FilmRepository {
+    private final JdbcClient jdbcClient;
+    public FilmRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    long findTotaalVrijePlaatsen(){
+        var sql= """
+                select sum(vrijePlaatsen) as totaalVrijePlaatsen
+                from films
+                """;
+        return jdbcClient.sql(sql)
+                .query(Long.class)
+                .single();
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/be/vdab/cinefest/films/FilmService.java
@@ -0,0 +1,20 @@
+package be.vdab.cinefest.films;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+class FilmService {
+    private final FilmRepository filmRepository;
+    public FilmService(FilmRepository filmRepository) {
+        this.filmRepository = filmRepository;
+    }
+
+    long findTotaalVrijePlaatsen() {
+        return filmRepository.findTotaalVrijePlaatsen();
+    }
+
+
+
+}
